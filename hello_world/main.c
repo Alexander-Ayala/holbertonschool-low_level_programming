026137b03d8207c6504d6f9bf0744cb6caa76e9a
Alexander-Ayala <ferchoalex@gmail.com>
2022-06-15 15:50:51 -0500

    script que ejecut archivo c

--- hello_world/main.c
+++ hello_world/main.c
@@ -2,6 +2,6 @@
 /* runs a C file preprocessor and save the result into another file*/
 int main(void) /*varaible gardada para ejecutar desde un script*/
 {
-printf('./a.out > c');
+printf("hello world");
 return (0);
 }
