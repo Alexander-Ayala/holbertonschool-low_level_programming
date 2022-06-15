434ce91d701588527f7e9952ba1feeab36058b70
Alexander-Ayala <ferchoalex@gmail.com>
2022-06-15 15:46:50 -0500

    script que ejecut archivo c

---
+++ hello_world/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+/* runs a C file preprocessor and save the result into another file*/
+int main(void) /*varaible gardada para ejecutar desde un script*/
+{
+printf('./a.out > c');
+return (0);
+}
