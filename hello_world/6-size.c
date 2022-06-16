9f35249b7e8c52f18a66506855c0cea3bc34949d
Alexander-Ayala <ferchoalex@gmail.com>
2022-06-16 01:50:56 -0500

    imprime size various type sizeof

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,1 +1,17 @@
-#!/bin/bash
+#include<stdio.h>
+#include<string.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int a = 16;
+printf("Size of variable a : %d\n", sizeof(a));
+printf("Size of variable a : %d\n", sizeof(int));
+printf("Size of variable a : %d\n", sizeof(char));
+printf("Size of variable a : %d\n", sizeof(float));
+printf("Size of variable a : %d\n", sizeof(double));
+return (0);
+}
