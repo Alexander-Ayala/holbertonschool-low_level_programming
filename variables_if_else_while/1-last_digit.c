7599054571d1af0d29ac30d8e2e47b2a11899a78
Alexander-Ayala <ferchoalex@gmail.com>
2022-06-17 01:50:37 -0500

    imprime ultimo number mayor5 menor 6 igual 0)

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -13,7 +13,7 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	{
+
 	printf("Last digit of  %d is %d ", n, n % 10);
 	if (n % 10 > 5)
 	{
@@ -27,6 +27,6 @@ int main(void)
 	{
 	printf("and is less than 6 and not 0\n");
 	}
-	}
+
 	return (0);
 }
