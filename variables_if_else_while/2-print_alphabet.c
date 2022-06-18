8859156d525d2663886991c86f72695c691c299d
Alexander-Ayala <ferchoalex@gmail.com>
2022-06-17 23:05:17 -0700

    imprimir alfabeto

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main - imprima alfabeto
+ *return: zero
+ *
+ */
+int main(void) /**main abecedario  */
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+putchar(c);
+}
+return (0);
+}
