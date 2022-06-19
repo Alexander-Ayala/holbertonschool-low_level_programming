bfb26f9f21f10f425dbe556216b5a4710cfe8776
Alexander-Ayala <ferchoalex@gmail.com>
2022-06-18 23:26:46 -0700

    imprima de 0 a 9 con coma y espacio

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - alfabeto minusculas mayusculas
+ * 3 putchar
+ * Return: zer0
+ *
+ */
+int main(void)
+{
+int c;
+int o = 44;
+int s = 32;
+for (c = 48; c <= 57; c++)
+{
+putchar(c);
+putchar(o);
+putchar(s);
+}
+putchar('\n');
+return (0);
+}
