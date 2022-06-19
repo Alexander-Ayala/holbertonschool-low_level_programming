46f380ac407d7b465ab7d574c3263872b0cc92c2
Alexander-Ayala <ferchoalex@gmail.com>
2022-06-18 22:26:44 -0700

    imprime de 0 9 ascii

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - alfabeto minusculas mayusculas
+ * 3 putchar
+ * Return: zer0
+ *
+ */
+int main(void)
+{
+int c;
+for (c = 48; c <= 57; c++)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
