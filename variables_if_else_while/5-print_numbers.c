1f9de286fb1ab50a092fdf9204757c9b14234da0
Alexander-Ayala <ferchoalex@gmail.com>
2022-06-18 21:09:29 -0700

    imprima de 0 a 10 con salto line

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - imprima o hasta 9 con salto line
+ * desde 0
+ *Return: 0
+ *
+ */
+int main(void)
+{
+int i = 0;
+while (i <= 10)
+{
+printf("%d\n", i);
+i= i+1;
+}
+return (0);
+}
