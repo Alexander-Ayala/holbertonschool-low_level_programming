b816fd67f21c4bb33f52c9525e3073201c7ef971
Alexander-Ayala <ferchoalex@gmail.com>
2022-06-20 15:09:27 -0700

    printf _putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,12 +1,18 @@
 #include "main.h"
 /**
- *_putchar - retornar 0
- *@c: imprimir caracter
- *
- *Return: On success 1
- *On error, -1 is returned
+ * main - imprime _putchar
+ *Return: 0
  */
-int main(void) 
+int main(void)
 {
+_putchar('_');  
+_putchar('p');  
+_putchar('u');
+_putchar('t');  
+_putchar('c');  
+_putchar('h');
+_putchar('a');  
+_putchar('r');
+_putchar('\n');
 return (0);
 }
