1c9d55f9fd7d2624f82e3a7e2fe0ad7078d2fff6
Alexander-Ayala <ferchoalex@gmail.com>
2022-06-19 20:56:51 -0700

    putchar imprima numeros con 3 digitos

---
+++ variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - prints all possible different combinations of three digits
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+int a;
+int i;
+int b;
+for (i = 48; i < 58; i++)
+{
+for (a = 49; a < 58; a++)
+{
+for (b = 50; b < 58; b++)
+{
+if (b > a && a > i)
+{
+putchar(i);
+putchar(a);
+putchar(b);
+if (i != 55 || a != 56)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
