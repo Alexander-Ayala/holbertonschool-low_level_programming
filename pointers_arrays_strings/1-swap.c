43ed25f71c44733bdcb896461b24764bdce69f1c
Alexander-Ayala <ferchoalex@gmail.com>
2022-06-22 12:32:03 -0700

    swaps intercambia the values of two integers.

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* swap_int - change values integedr
+* @*a: integer 1
+* @*b: integer 2
+*@a: numeros
+*@b: int
+**/
+void swap_int(int *a, int *b)
+{
+int c;
+c = *a;
+*a = *b;
+*b = c;
+}
