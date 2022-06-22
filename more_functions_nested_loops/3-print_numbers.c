c5c96ee0890902eaa4bcd993271fccab19757c35
Alexander-Ayala <ferchoalex@gmail.com>
2022-06-21 20:01:08 -0700

    prints the numbers, from 0 to 9, followed by a new line putchar

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* print_numbers - printnumbers 0 9
+* Return: 0
+*/
+void print_numbers(void)
+{
+int i;
+i = '0';
+while (i <= '9')
+{
+_putchar(i);
+i++;
+}
+_putchar('\n');
+}
