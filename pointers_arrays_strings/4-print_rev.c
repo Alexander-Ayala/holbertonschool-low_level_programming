45e410869aa64d81746daf62f54b6f5e603895f7
Alexander-Ayala <ferchoalex@gmail.com>
2022-06-23 13:36:03 -0700

    imprime cadena rev

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+* print_rev - print reverse string
+* @*s: pointer string
+* @s: string
+*
+**/
+void print_rev(char *s)
+{
+int i = 0;
+while (*(s + i))
+{
+i++;
+}
+i = i-1;
+while (i >= 0)
+{
+_putchar(*(s + i));
+i--;
+}
+_putchar('\n');
+}
