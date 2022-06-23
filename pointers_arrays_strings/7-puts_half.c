1a2a32dfb79edf97e6c8170e13b2652ef9755d9e
Alexander-Ayala <ferchoalex@gmail.com>
2022-06-23 15:28:44 -0700

    imprime la mitad

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* puts_half - print second half of the string
+* @*str: pointer string
+* @str: string
+*
+**/
+void puts_half(char *str)
+{
+int i, half;
+i = 0;
+half = 0;
+while (*(str + i))
+{
+i++;
+}
+if (i % 2 == 0)
+{
+half = i / 2;
+}
+while (half < i)
+{
+_putchar(*(str + half));
+half++;
+}
+_putchar('\n');
+}
