b5e99898495113f7654d915f73c72c2d2abe9b4a
Alexander-Ayala <ferchoalex@gmail.com>
2022-06-30 13:27:32 -0500

    palabras con mayuscula

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -8,10 +8,23 @@
  */
 char *cap_string(char *c)
 {
-int i;
-for (i = 0; c[i] != '\0'; i++)
-{
-c[i] = toupper(c[i]);
-}
-return (c);
+	int i, j;
+	char ch[13] = {' ', '\t', '\n', ',', ';', '.',
+	'!', '?', '"', '(', ')', '{', '}'};
+	for (i = 0; c[i] != '\0'; i++)
+	{
+	if (i == 0 && c[i] >= 'a' && c[i] <= 'z')
+	c[i] -= 32;
+	for (j = 0; j < 13; j++)
+	{
+	if (c[i] == ch[j])
+	{
+	if (c[i + 1] >= 'a' && c[i + 1] <= 'z')
+	{
+	c[i + 1] -= 32;
+	}
+	}
+	}
+	}
+	return (c);
 }
