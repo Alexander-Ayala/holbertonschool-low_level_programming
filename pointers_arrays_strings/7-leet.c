3a4d3f590373bcc11a202ba952012d9ee13124c4
Alexander-Ayala <ferchoalex@gmail.com>
2022-07-01 22:47:30 -0500

    that encodes a string into 1337

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * leet - codiing string 1337
+ * @s: string
+ * Return: coding c
+ */
+char *leet(char *c)
+{
+	int i, j;
+
+	char *x = "aAeEoOtTlL";
+	char *y = "4433007711";
+
+	for (i = 0; c[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (c[i] == x[j])
+			{
+				c[i] = y[j];
+			}
+		}
+	}
+	return (c);
+}
