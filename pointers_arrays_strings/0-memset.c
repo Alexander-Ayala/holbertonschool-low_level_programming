1c4b03f6289c971bed927934e9a8a55613dd94a5
Alexander-Ayala <ferchoalex@gmail.com>
2022-07-04 20:49:17 -0500

    fills memory with a constant byte

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * _memset - consttant byte memory
+ * @s: poiter character
+ * @b: character b
+ * @n: n times
+ * Return: s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int a;
+
+	for (a = 0; a < n; a++)
+	{
+		s[a] = b;
+	}
+
+	return (s);
