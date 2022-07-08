aa62d1e680a1e69e2e41ee84ef8dc74bf84292a8
Alexander-Ayala <ferchoalex@gmail.com>
2022-07-07 19:03:18 -0500

    evuelve un puntero a un espacio recién asignado en la memoria, que contiene una copia de la cadena dada como parámetro

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+/**
+ * _strdup - poiter
+ * @str: argumen string
+ * Return: string copy
+ */
+char *_strdup(char *str)
+{
+unsigned int i = 0;
+char *scpy = 0;
+scpy = malloc(sizeof(str));
+if (scpy == NULL)
+	return (NULL);
+i = 0;
+while (str[i] != '\0')
+{
+scpy[i] = str[i];
+i++;
+}
+return (scpy);
+}
