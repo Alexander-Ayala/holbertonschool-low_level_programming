56da08ae11c9dab8ac13b014fa84adaa019223d4
Alexander-Ayala <ferchoalex@gmail.com>
2022-07-12 18:48:25 -0500

    type struct dog with the following elements: name,age,float,owner,char

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,5 +1,11 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog - dog.h
+ * @name: type element pointer char
+ * @age: type element float
+ * @owner: type element pointer char
+ */
 struct dog
 {
 char *name;
